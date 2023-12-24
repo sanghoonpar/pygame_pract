d961c1529649111a962c61f52e68cddbd29e6896
sanghoonpar <parkcs11sh@gmail.com>
2023-12-24 12:23:10 +0900

    mole_pos 지정

--- whack_a_mole_game/whack_a_mole_game.py
+++ whack_a_mole_game/whack_a_mole_game.py
@@ -10,34 +10,43 @@ import random
 pg.init()
 
 screen = pg.display.set_mode((800, 600))
-background_img = pg.image.load("whack_a_mole_game/src/background.png")
-mole_img = pg.image.load("whack_a_mole_game/src/mole.png")
+background_img = pg.image.load("pygame_pract/whack_a_mole_game/src/background.png")
+mole_img = pg.image.load("pygame_pract/whack_a_mole_game/src/mole.png")
 running = True
 score = 0
 font = pg.font.SysFont("nanumbarungothic", 50)
-mole_pos = [(25, 45), (200, 45), (370, 45), (540, 45), (25, 175), (200, 175), (370, 175), (540, 175), (25, 305), (200, 305), (370, 305), (540, 305)]
+mole_pos = [(35, 70), (210, 70), (380, 70), (550, 70), (35, 210), (210, 210), (380, 210), (550, 210), (35, 340), (210, 340), (380, 340), (550, 340)]
 
-print('test')
+
 while running:
     time = pg.time.get_ticks() // 1000
+    time_text = font.render(str(time), True, (0, 0, 0), None)
+    score_text = font.render(str(score), True, (0, 0, 0), None)
+    moles = []
+    check_time = True
     
     for event in pg.event.get():
-        check_time = False
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
-            score += 1
+            if mole.collidepoint(event.pos):
+                score += 1
+                moles.remove(mole) 
+                break
 
-    if pg.time.get_ticks() // 1000 % 2 == 0:
+    if time // 1000 % 2 == 0:
         if check_time == True:
-            screen.blit(mole_img, random.choice(mole_pos))
+            add_mole = screen.blit(mole_img, random.choice(mole_pos))
             pg.display.update() 
-            pg.time.wait(500)
+            pg.time.wait(2000)
+            moles.append(add_mole)
             check_time = False
     else:
         check_time = True
-    time_text = font.render(str(time), True, (0, 0, 0), None)
-    score_text = font.render(str(score), True, (0, 0, 0), None)
+    
+    for mole in moles:
+        screen.blit(mole_img,mole)
+        pg.display.update()
     
     if time > 20:
             if score >= 10:

5c7c54999497deb16c6a6bd3221fd642a45ec96d
sanghoonpar <parkcs11sh@gmail.com>
2024-01-06 13:28:12 +0900

    whack a mole게임 완성

--- whack_a_mole_game/whack_a_mole_game.py
+++ whack_a_mole_game/whack_a_mole_game.py
@@ -12,16 +12,20 @@ pg.init()
 screen = pg.display.set_mode((800, 600))
 background_img = pg.image.load("pygame_pract/whack_a_mole_game/src/background.png")
 mole_img = pg.image.load("pygame_pract/whack_a_mole_game/src/mole.png")
+hammer_img = pg.image.load("pygame_pract/whack_a_mole_game/src/hammer.png")
+hammer_img = pg.transform.scale(hammer_img, (150, 150))
 running = True
+vic_img = pg.image.load("pygame_pract/whack_a_mole_game/src/victory.jpg")
+los_img = pg.image.load("pygame_pract/whack_a_mole_game/src/lose.jfif")
 score = 0
 font = pg.font.SysFont("nanumbarungothic", 50)
 mole_pos = [(35, 70), (210, 70), (380, 70), (550, 70), (35, 210), (210, 210), (380, 210), (550, 210), (35, 340), (210, 340), (380, 340), (550, 340)]
 check_time = True
 moles = []
+pg.mouse.set_visible(False)
 
 while running:
     screen.blit(background_img, background_img.get_rect())
-
     time = pg.time.get_ticks() // 1000
     time_text = font.render(str(time), True, (0, 0, 0), None)
     score_text = font.render(str(score), True, (0, 0, 0), None)
@@ -53,14 +57,19 @@ while running:
 
     if time > 20:
         if score >= 10:
-            print("victory")
+            pg.time.delay(500)
+            screen.blit(vic_img, vic_img.get_rect())
+            pg.time.delay(3000)
             running = False
         else:
-            print("lose")
+            pg.time.delay(500)
+            screen.blit(los_img, los_img.get_rect())
+            pg.time.delay(3000)
             running = False
 
     screen.blit(time_text, (680, 55))
     screen.blit(score_text, (55, 55))
+    screen.blit(hammer_img, pg.mouse.get_pos())
     pg.display.update()
 
 pg.quit()
